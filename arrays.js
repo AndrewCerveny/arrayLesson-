0c196c126a32753306d3d1634bf3e1dc7d464b3c
Andrew Cerveny <andrewfcerveny0628@gmail.com>
2022-08-03 14:00:38 -0400

    Initial commit

---
+++ arrays.js
@@ -0,0 +1,11 @@
+// Array Lesson Homework
+//Declare a var Array that stores at least 4 strings
+var roygbiv = ["red","orange","yellow","green","blue"," indigo","violet"];
+
+//declare a var that stores an array of atleast 4 Numbers
+var players = [1,2,3,4,5,];
+
+//declare an array that stores at least 4 booleans
+
+var employed = [true,false,false,true];
+ 
