a7d2a27c671b7aba25cc2930465206b2b7999409
Andrew Cerveny <andrewfcerveny0628@gmail.com>
2022-08-03 16:02:13 -0400

    Add arr methods and explain

--- arrays.js
+++ arrays.js
@@ -8,4 +8,15 @@ var players = [1,2,3,4,5,];
 //declare an array that stores at least 4 booleans
 
 var employed = [true,false,false,true];
- 
+
+ // Call different methods on arrays
+ console.log(roygbiv.push("black"));
+ console.log(roygbiv);
+ // .push() adds one more element "black" and to original arry (line14)
+ //  .push() also retuns new length of array 8
+
+ console.log(players.shift()); //should remove 1st element /0 index <1> and return 1
+ console.log(players);// should have orignal arry minus the element <1>
+
+console.log(employed.pop()); // should remove last element <true> and return <true>
+console.log(employed);// reflect original arry with <3> elements instead of <4>  
